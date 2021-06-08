e4afaedd054a8a592b9c6cd6d08b0db61de4a91a
Tracy Dong <dongtracy2002@gmail.com>
2021-06-08 19:14:50 -0400

    added basic Flask routes and HTML pages

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,19 @@ load_dotenv()
 app = Flask(__name__)
 
 
+# ROUTES
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/profiles')
+def my_profiles():
+    return render_template('profiles.html', title="Meet the Fellows", url=os.getenv("URL"))
+
+@app.route('/wishlist')
+def my_wishlist():
+    return render_template('wishlist.html', title="Our Wishlists", url=os.getenv("URL"))
+
+@app.route('/blog')
+def my_blog():
+    return render_template('blog.html', title="Blog", url=os.getenv("URL"))
