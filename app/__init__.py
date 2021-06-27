1fd7776b3ccd31cba67a7bbd01f60e4d79433418
Cloud User <centos@ip-172-31-22-118.us-west-1.compute.internal>
2021-06-27 03:23:44 +0000

    add sql db

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,28 @@
 import os
-from flask import Flask, render_template, send_from_directory
-from dotenv import load_dotenv
+from flask import Flask, render_template
+from . import db
 
-load_dotenv()
 app = Flask(__name__)
+app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
+db.init_app(app)
 
+# from dotenv import load_dotenv
+
+# load_dotenv()
+# app = Flask(__name__)
 
 # ROUTES
-@app.route('/')
-def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+# @app.route('/')
+# def index():
+#     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
 
-@app.route('/profiles')
-def my_profiles():
-    return render_template('profiles.html', title="Meet the Fellows", url=os.getenv("URL"))
+# @app.route('/profiles')
+# def my_profiles():
+#     return render_template('profiles.html', title="Meet the Fellows", url=os.getenv("URL"))
 
-@app.route('/wishlist')
-def my_wishlist():
-    return render_template('wishlist.html', title="Our Wishlists", url=os.getenv("URL"))
+# @app.route('/wishlist')
+# def my_wishlist():
+#     return render_template('wishlist.html', title="Our Wishlists", url=os.getenv("URL"))
 
 @app.route('/blog')
 def my_blog():
